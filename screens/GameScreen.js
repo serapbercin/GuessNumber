69d8c98913e9104845799f68e1dd77de4730fa2e
serap bercin <serapbercin@seraps-MBP.fritz.box>
2020-09-16 01:14:25 +0200

    Adding Hints and Validation

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { View, StyleSheet, Text, Button, Alert } from 'react-native'
 
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
@@ -22,6 +22,24 @@ const generateRandomBetween = (min, max, exclude) => {
 const GameScreen = props => {
 
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+    const currentLow = useRef(1)
+    const currentHigh = useRef(100)
+
+    const nextGuessHandler = direction => {
+        if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
+            Alert.alert('Don\'t lie!', "you know that this is wrong....", [{ text: 'Sorry', style: 'cancel' }])
+            return
+        }
+
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess
+        } else {
+            currentLow.current = currentGuess
+        }
+
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
+        setCurrentGuess(nextNumber)
+    }
 
     return (<View style={styles.screen}>
         <Text>
@@ -31,9 +49,9 @@ const GameScreen = props => {
             {currentGuess}
         </NumberContainer>
         <Card style={styles.buttonContainer}>
-            <Button title="Lower" onPress={() => { }}></Button>
+            <Button title="Lower" onPress={nextGuessHandler.bind(this, 'lower')}></Button>
 
-            <Button title="Greater" onPress={() => { }}></Button>
+            <Button title="Greater" onPress={nextGuessHandler.bind(this, 'greater')}></Button>
 
 
         </Card>
